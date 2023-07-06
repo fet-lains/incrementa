c2eda6c4587c930fe31fd9671771ac48d0773b30
Fedor Sharaputo <sharaputo@icloud.com>
2023-07-06 10:08:13 +0300

    add Capitalize helper function

--- src/composables/helpers.ts
+++ src/composables/helpers.ts
@@ -1,3 +1,7 @@
+export const capitalize = (string: string) => {
+  return string[0].toUpperCase() + string.substring(1).toLowerCase();
+};
+
 export const isDesktop = () => {
   const desktopRes = window.matchMedia('(min-width: 1200px)');
   const mousePointer = window.matchMedia('(pointer: fine)');
